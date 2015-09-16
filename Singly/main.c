b037361e7ba7029b2273a023b1e624cda7a8ebd8
Vishal Musale <vishal.m.musale@gmail.com>
2015-09-16 15:29:58 -0700

    Initial Commit
    Incomplete Linked list

---
+++ Singly/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    // Any data type can be stored in this node
+    int data;
+
+    struct node *next;
+};
+
+void push(struct node** head, int data)
+{
+    // Allocate memory for node
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+
+    temp->data  = malloc(data_size);
+    temp->next = (*head);
+
+    // Copy contents of new_data to newly allocated memory.
+    // Assumption: char takes 1 byte.
+    int i;
+    for (i=0; i<data_size; i++)
+        *(char *)(temp->data + i) = *(char *)(data + i);
+
+    // Change head pointer as new node is added at the beginning
+    (*head)    = temp;
+}
+
+/* Function to print nodes in a given linked list. fpitr is used
+   to access the function to be used for printing current node data.
+   Note that different data types need different specifier in printf() */
+void printList(struct node *node, void (*fptr)(void *))
+{
+    while (node != NULL)
+    {
+        (*fptr)(node->data);
+        node = node->next;
+    }
+}
+
+// Function to print an integer
+void printInt(void *n)
+{
+   printf(" %d", *(int *)n);
+}
+
+// Function to print a float
+void printFloat(void *f)
+{
+   printf(" %f", *(float *)f);
+}
+
+/* Driver program to test above function */
+int main()
+{
+    struct node *start = NULL;
+
+    // Create and print an int linked list
+    unsigned int_size = sizeof(int);
+    int arr[] = {10, 20, 30, 40, 50}, i;
+    for (i=4; i>=0; i--)
+       push(&start, &arr[i], int_size);
+    printf("Created integer linked list is \n");
+    printList(start, printInt);
+
+    // Create and print a float linked list
+    unsigned float_size = sizeof(float);
+    start = NULL;
+    float arr2[] = {10.1, 20.2, 30.3, 40.4, 50.5};
+    for (i=4; i>=0; i--)
+       push(&start, &arr2[i], float_size);
+    printf("\n\nCreated float linked list is \n");
+    printList(start, printFloat);
+
+    return 0;
+}
